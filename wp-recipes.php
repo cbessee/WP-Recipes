bc1dbbf69fdf26ce4e1591d2e9bebabbe7c672bd
Cole Bessee <cbessee@uw.edu>
2016-03-06 09:36:33 -0800

    Add base plugin file and appropriate headers

---
+++ wp-recipes.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: WP-Recipes
+Plugin URI:  https://github.com/cbessee/WP-Recipes
+Description: A simple wordpress plugin for creating custom recipe post types
+Version:     1.0
+Author:      Cole Bessee
+Author URI:  https://github.com/cbessee
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+WP-Recipes is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+WP-Recipes is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with WP-Recipes. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+
+
+?>
